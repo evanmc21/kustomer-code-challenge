6fb6eca4731fe779b541fa52324eb68354f574b2
evanmc21 <emcandler21@gmail.com>
2018-09-18 14:34:56 -0400

    added arrow functions and cleaned upp code

--- kustomer-code-challenge/app.js
+++ kustomer-code-challenge/app.js
@@ -2,8 +2,8 @@ const fs = require('fs');
 const csv = require('fast-csv');
 const axios = require("axios");
 const URL = "https://api.kustomerapp.com/v1/customers"
+/* I would store an API key in a local environment variable that will be kept in a file not included in git */
 const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjViOWZjNmY1ODk5NWVjMDAxMDgwZDQ1OSIsInVzZXIiOiI1YjlmYzZmNTVhNjgyNjVmOWI5OTY1ZmMiLCJvcmciOiI1YjljMmQzZjRlZDEyNmRmNDRkNTBkNjkiLCJvcmdOYW1lIjoienp6LWV2YW4iLCJ1c2VyVHlwZSI6Im1hY2hpbmUiLCJyb2xlcyI6WyJvcmcuYWRtaW4iLCJvcmcudXNlciJdLCJleHAiOjE1Mzc4MDI2MTIsImF1ZCI6InVybjpjb25zdW1lciIsImlzcyI6InVybjphcGkiLCJzdWIiOiI1YjlmYzZmNTVhNjgyNjVmOWI5OTY1ZmMifQ.Oq6UDenPjg0nz_xPIhnIDX-o0qyLdJypcqt4aiEORZs'
-
 /* make csv data readable */
 fs.createReadStream('customers.csv')
   .pipe(csv())
@@ -25,18 +25,13 @@ fs.createReadStream('customers.csv')
           phone: data[5]
         }
     ]
-  }
-    // console.log(customer);
+  };
   axios({
     method: "POST",
     url: `${URL}`,
     headers: {"Authorization":`Bearer ${token}`},
     data: customer
   })
-  .then(function(response){
-    console.log(response.data);
-  })
-  .catch(function(error){
-    console.log(error);
-  });
+  .then(resp => console.log(resp.data))
+  .catch(console.log)
 });
