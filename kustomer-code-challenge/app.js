cda37f063c4855bfb457f71b8c404d66efe94cbb
evanmc21 <emcandler21@gmail.com>
2018-09-18 12:59:59 -0400

    added comments and cleaned up syntax

--- kustomer-code-challenge/app.js
+++ kustomer-code-challenge/app.js
@@ -1,13 +1,14 @@
 const fs = require('fs');
-const csv = require('fast-csv')
-const axios = require("axios")
+const csv = require('fast-csv');
+const axios = require("axios");
 const URL = "https://api.kustomerapp.com/v1/customers"
 const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjViOWZjNmY1ODk5NWVjMDAxMDgwZDQ1OSIsInVzZXIiOiI1YjlmYzZmNTVhNjgyNjVmOWI5OTY1ZmMiLCJvcmciOiI1YjljMmQzZjRlZDEyNmRmNDRkNTBkNjkiLCJvcmdOYW1lIjoienp6LWV2YW4iLCJ1c2VyVHlwZSI6Im1hY2hpbmUiLCJyb2xlcyI6WyJvcmcuYWRtaW4iLCJvcmcudXNlciJdLCJleHAiOjE1Mzc4MDI2MTIsImF1ZCI6InVybjpjb25zdW1lciIsImlzcyI6InVybjphcGkiLCJzdWIiOiI1YjlmYzZmNTVhNjgyNjVmOWI5OTY1ZmMifQ.Oq6UDenPjg0nz_xPIhnIDX-o0qyLdJypcqt4aiEORZs'
 
+/* make csv data readable */
 fs.createReadStream('customers.csv')
   .pipe(csv())
   .on('data', function(data){
-    /*each row is an object*/
+    /* each row is an object */
     let customer = {
       name: data[0] +' '+ data[1],
       emails: [
